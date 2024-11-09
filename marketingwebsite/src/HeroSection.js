00f699e18ab17a9a497648a9ce43ebfd5bc78c13
wayne-DISM <chewwayne22@gmail.com>
2024-11-09 23:09:11 +0800

    HeroSection is dyanamic

--- marketingwebsite/src/HeroSection.js
+++ marketingwebsite/src/HeroSection.js
@@ -3,8 +3,8 @@ import React from 'react';
 import HeroMainImage from './images/test.png';
 
 const HeroSection = () => (
-  <div className="hero-section-container">
-    <div className="hero">
+  <div className="hero">
+    <div className="hero-text">
       <h1>Ask<span>Vox</span></h1>
       <p>A voice-activated knowledge assistant designed to support gamers with real-time information without interrupting gameplay.</p>
     </div>
